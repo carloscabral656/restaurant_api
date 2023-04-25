25dbdd6c390ab1c3b0b96684a147ff2ce089b709
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-04-24 22:55:52 -0300

    Creating some migrations

---
+++ database/migrations/2023_04_25_003303_create_address_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id();
+            $table->string('street');
+            $table->integer('number');
+            $this->string('neighborhood');
+            $this->string('city');
+            $this->char('state', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+};
