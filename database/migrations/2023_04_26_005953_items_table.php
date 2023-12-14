09c05293ddc276c5944a5eacb6192ea4f96de80c
Carlos Eduardo <edu.carlos.cec@gmail.com>
2023-12-14 09:03:35 -0300

    Creating type_item in items table

--- database/migrations/2023_04_26_005953_items_table.php
+++ database/migrations/2023_04_26_005953_items_table.php
@@ -15,16 +15,19 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+
             $table->unsignedBigInteger("id_menu");
+            $table->foreign("id_menu")->references("id")->on("menus")->cascadeOnDelete();
+
+            $table->unsignedBigInteger('id_type_item');
+            $table->foreign("id_type_item")->references("id")->on("type_item")->cascadeOnDelete();
+
             $table->string("name");
             $table->string('description');
             $table->string('img_item');
             $table->float('unit_price');
             $table->float('discount');
-            $table->foreign("id_menu")
-                ->references("id")
-                ->on("menus")
-                ->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
